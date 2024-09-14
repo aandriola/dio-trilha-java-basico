fe7451936ea1ea414e0bc2a415f52ffb865aa35c
aandriola <adrianoandriola.alexandre@gmail.com>
2024-09-14 09:30:47 -0300

    Inserindo Metodos para validar dados de entrada

--- ContaBanco/ContaBanco/src/ContaBanco.java
+++ ContaBanco/ContaBanco/src/ContaBanco.java
@@ -7,7 +7,7 @@ public class ContaBanco {
         System.out.println("Olá Vamos iniciar a criação de sua conta!");
         
         System.out.print("Por favor, Informe o codigo da sua conta EX 1021 : ");
-        int conta = scanner.nextInt();
+        int conta = lerInteiroPositivo(scanner);
 
         System.out.print("Agora Informe a agência EX 067-8 : ");
         String agencia = scanner.next();
@@ -23,4 +23,18 @@ public class ContaBanco {
         System.out.println("Olá " + nome + " , obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + conta + " e seu saldo R$"+ saldo +" já está disponível para saque");
 
     }
+        
+        private static int lerInteiroPositivo(Scanner scanner) {
+            int valor;
+            do {
+                while (!scanner.hasNextInt()) {
+                    System.out.print("Valor inválido. Digite um número inteiro positivo: ");
+                    scanner.next();
+                }
+                valor = scanner.nextInt();
+            } while (valor <= 0);
+            return valor;
+        }
+
+    
 }
